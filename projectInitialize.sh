3033ea0e4d84ce149c0e1b8608652fa5962a8916
binaya-khadka <binayamm@gmail.com>
2023-02-27 20:47:54 +0545

    project_initialize_changes

--- projectInitialize.sh
+++ projectInitialize.sh
@@ -2,7 +2,6 @@ composer install
 cp .env.example .env
 php artisan migrate:fresh --seed
 php artisan key:generate
-composer dump:autoload
 php artisan view:clear
 php artisan cache:clear
 php artisan config:clear
