83f781f84ad552949135a3c147a34788263021bf
nirajuprety <nerajupreti@gmail.com>
2023-03-16 15:41:32 +0545

    homepage added

---
+++ app/Pickles/executeScripts.php
@@ -0,0 +1,8 @@
+// Execute the Python script and capture its output
+$output = exec('python path/to/python/script.py');
+
+// Decode the JSON string
+$data = json_decode($output, true);
+
+// Use the data in your Laravel application's code
+var_dump($data);
