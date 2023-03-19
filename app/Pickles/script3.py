59d6cf3b81441ed19123d50df5f20e0bb3c526fa
nirajuprety <nerajupreti@gmail.com>
2023-03-19 22:02:11 +0545

    niraj edit

--- app/Pickles/script3.py
+++ app/Pickles/script3.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np;
 import pandas as pd
 import os
+import sys
 
 def gaussian_pdf(x, mean, std):
     """Compute the probability density function of a Gaussian distribution"""
@@ -37,61 +38,36 @@ with open(model_file_path, 'rb') as f:
     model = pickle.load(f)
 
 # Take input from the user
-# age = 48 #int(input("Enter Age: "))
-# bp =  80 #int(input("Enter Blood Pressure: "))
-# sg =  1.02 #float(input("Enter Specific Gravity (1.005,1.010,1.015,1.020,1.025): "))
-# al =  1 #int(input("Enter Albumin (0,1,2,3,4,5): "))
-# su =  0 #int(input("Enter Sugar (0,1,2,3,4,5): "))
-# rbc =  1 #input("Enter Red Blood Cells (normal,abnormal): ")
-# pc = 1 #input("Enter Pus Cell (normal,abnormal): ")
-# pcc = 1 #input("Enter Pus Cell Clumps (present,notpresent): ")
-# ba =  1 #input("Enter Bacteria (present,notpresent): ")
-# bgr = 121 #int(input("Enter Blood Glucose Random: "))
-# bu = 36 #int(input("Enter Blood Urea: "))
-# sc = 1.2 #float(input("Enter Serum Creatinine: "))
-# sod = 135 #int(input("Enter Sodium: "))
-# pot = 4.8 #float(input("Enter Potassium: "))
-# hemo = 15.4 #float(input("Enter Hemoglobin: "))
-# pcv =  44 #int(input("Enter Packed Cell Volume: "))
-# wc =  7800#int(input("Enter White Blood Cell Count: "))
-# rc =  4.2 #float(input("Enter Red Blood Cell Count: "))
-# htn = 1 #input("Enter Hypertension (yes,no): ")
-# dm = 1#input("Enter Diabetes Mellitus (yes,no): ")
-# cad = 1 #input("Enter Coronary Artery Disease (yes,no): ")
-# appet = 1#input("Enter Appetite (good,poor): ")
-# pe = 1#input("Enter Pedal Edema (yes,no): ")
-# ane = 1#input("Enter Anemia (yes,no): ")
-
-# age = 30
-# bp = 80
-# sg = 1.020
-# al = 0
-# su = 0
-# rbc = 1
-# pc = 1
-# pcc = 0
-# ba = 0
-# bgr = 80
-# bu = 25
-# sc = 0.7
-# sod = 140
-# pot = 4.5
-# hemo = 14.5
-# pcv = 44
-# wc = 7500
-# rc = 5.2
-# htn = 0
-# dm = 0
-# cad = 0
-# appet = 0
-# pe = 0
-# ane = 0
+age = int(sys.argv[1])
+bp = int(sys.argv[2])
+sg = float(sys.argv[3])
+al = int(sys.argv[4])
+su = int(sys.argv[5])
+rbc = sys.argv[6]
+pc = sys.argv[7]
+pcc = sys.argv[8]
+ba = sys.argv[9]
+bgr = int(sys.argv[10])
+bu = int(sys.argv[11])
+sc = float(sys.argv[12])
+sod = int(sys.argv[13])
+pot = float(sys.argv[14])
+hemo = float(sys.argv[15])
+pcv = int(sys.argv[16])
+wc = int(sys.argv[17])
+rc = float(sys.argv[18])
+htn = sys.argv[19]
+dm = sys.argv[20]
+cad = sys.argv[21]
+appet = sys.argv[22]
+pe = sys.argv[23]
+ane = sys.argv[24]
 
 # Process the input using the loaded model
 # X = np.array([[age, bp, sg, al, su, rbc, pc, pcc, ba, bgr, bu, sc, sod, pot, hemo, pcv, wc, rc, htn, dm, cad, appet, pe, ane]])
 # X = np.array([[50, 80, 1.015, 0, 1, 1, 0, 0, 0,  219, 176, 13.8, 136, 4.5, 8.6, 24, 13200, 2.7, 1, 0, 0, 0, 1,1]])
 
-X = pd.DataFrame([[58.0,80.0,1.025,0.0,0.0,1,1,0,0,131.0,18.0,1.1,141.0,3.5,15.8,53.0,6800.0,6.1,0,0,0,0,0,0]])
+X = pd.DataFrame([[age, bp, sg, al, su, rbc, pc, pcc, ba, bgr, bu, sc, sod, pot, hemo, pcv, wc, rc, htn, dm, cad, appet, pe, ane]])
 
 # print(X)
 y_pred = model.predict(X)
